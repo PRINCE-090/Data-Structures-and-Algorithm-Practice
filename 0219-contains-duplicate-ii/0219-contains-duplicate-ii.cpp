7e13e58c1a3659e74b41cafa411875efc9058546
Prince <24BAI70733@cuchd.in>
2026-07-11 15:22:38 +0530

    Time: 74 ms (57.43%), Space: 93.7 MB (89.32%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
+        unordered_set<int>s;
         int n = nums.size();
-        for(int i = 0;i<n;i++){
-          if(mp.count(nums[i])){
-            if(i - mp[nums[i]] <= k){
-              return true;
-            }
-          } 
-          mp[nums[i]] = i;
+       for(int i = 0;i<n;i++){
+        if(i > k){
+          s.erase(nums[i-k-1]);
         }
+        if(s.count(nums[i])) return true;
+          s.insert(nums[i]);
+       }
         return false;
     }
 };
