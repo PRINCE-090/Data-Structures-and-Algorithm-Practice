adba129f53f4923fb4a02d6fe8b5140a402690d8
Prince <24BAI70733@cuchd.in>
2026-07-15 13:53:31 +0530

    Time: 0 ms (100%), Space: 12.6 MB (11.11%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+   int i = m-1, j = n-1, k = m+n-1;
+   while(i >= 0 && j >= 0){
+    if(nums2[j] > nums1[i]){
+      swap(nums2[j] ,nums1[k]);
+      j--,k--;
+    }
+     else{
+      swap(nums1[i] , nums1[k]);
+      i--, k--;
+     }
+   }
+   while(j >= 0){
+    swap(nums2[j],nums1[k]);
+    k--;
+    j--;
+   }
+  }
+};
