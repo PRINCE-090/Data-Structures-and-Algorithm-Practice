6bdc6c5a19afa97b34aba9b6c3b303fc5de5d588
Prince <kumarsinghprince650@gmail.com>
2026-07-21 10:44:39 +0530

    Optimize floorSqrt function using binary search
    Refactor floorSqrt function to use binary search for efficiency.

--- Difficulty: Easy/Square Root/square-root.cpp
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -1,12 +1,21 @@
 class Solution {
   public:
     int floorSqrt(int n) {
-        int sq = 0;
-        for(int i = 1;i*i <= n;i++){
-             sq = i;
-            
-        }
+        if(n == 0 || n==1) return n;
+      long long left = 1,right = n/2;
+      int sq = 0;
+      while(left <= right){
+          long long mid = left+(right-left)/2;
+          if(mid*mid == n) return mid;
+          else if(mid*mid > n){
+              right = mid-1;
+          }
+          else{
+              sq = mid;
+              left = mid+1;
+          }
+      }
+      
         return sq;
-        
     }
 };
