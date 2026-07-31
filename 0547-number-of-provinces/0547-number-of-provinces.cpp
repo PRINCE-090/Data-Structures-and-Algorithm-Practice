a05fcfb4282c74c4e73f0de4550818a31dacb8e3
Prince <kumarsinghprince650@gmail.com>
2026-07-31 22:11:00 +0530

    Implement BFS to find number of provinces

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -21,3 +21,33 @@ public:
        return cnt;
     }
 };
+
+#bfs 
+void bfs(int node,vector<vector<int>>&arr,vector<int>&vis){
+        vis[node] = 1;
+       queue<int>q;
+       q.push(node);
+       while(!q.empty()){
+        int val = q.front();
+        q.pop();
+        for(int i = 0;i<arr.size();i++){
+            if(arr[val][i] == 1 && vis[i] == 0){
+                vis[i] = 1;
+                q.push(i);
+            }
+        }
+       }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+       int n = isConnected.size();
+       vector<int>vis(n+1,0);
+       int cnt = 0;
+       for(int i = 0;i<n;i++){
+        if(!vis[i]){
+            cnt++;
+            bfs(i,isConnected,vis);
+        }
+       }
+       return cnt;
+    }
+};
