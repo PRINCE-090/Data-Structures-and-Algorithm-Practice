1de5e00de3ebb4cc38bc527003b026e78383aa5c
Prince <24BAI70733@cuchd.in>
2026-08-05 13:38:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long power = n;
+        if(power < 0) power = -1*power;
+        while(power){
+            if(power%2){
+                ans = ans*x;
+                power = power-1;
+            }
+            else{
+                x = x*x;
+                power = power/2;
+            }
+        }
+        if(n < 0) ans = (double)(1.0)/(double)(ans);
+        return ans;
+    }
+};
