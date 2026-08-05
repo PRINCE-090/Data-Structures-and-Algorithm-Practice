ac0586710695ac3f3eb2bdecc8290ac39427e12c
Prince <kumarsinghprince650@gmail.com>
2026-08-05 11:05:06 +0530

    Add recursive Atoi function for string to integer conversion
    Implemented a recursive Atoi function to convert strings to integers.

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -23,3 +23,29 @@ public:
 
     }
 };
+
+
+ int Atoi(int i,string &s,long long &val,int sign){
+     if(i == s.size()) return sign*val;
+     if(!isdigit(s[i])) return sign* val;
+     val = (val*10)+(s[i]-'0');
+     if(sign *val > INT_MAX )return INT_MAX;
+     if(sign *val < INT_MIN )return INT_MIN;
+     return Atoi(i+1,s,val,sign);
+   }
+    int myAtoi(string s) {
+        int n = s.size();
+        long long res = 0,sign = 1,i = 0;
+        while(i < n && s[i] == ' ') i++;
+        if(i == s.size()) return 0;
+        if(s[i] == '-'){
+            sign = -1;
+            i++;
+        }
+        else if(s[i] == '+'){
+            i++;
+        }
+        long long val = 0;
+         return Atoi(i,s,val,sign);
+
+    }
