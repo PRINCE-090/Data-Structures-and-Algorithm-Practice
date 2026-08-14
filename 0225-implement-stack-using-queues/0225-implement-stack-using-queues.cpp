842241c10e2cc4f9389d734d846faebe3318de0e
Prince <kumarsinghprince650@gmail.com>
2026-08-14 13:30:08 +0530

    Add MyStack implementation using two queues
    Implemented a stack using two queues with push, pop, top, and empty methods.

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,3 +1,40 @@
+// using two queue
+class MyStack {
+public:
+     queue<int>q1,q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1,q2);
+    }
+    
+    int pop() {
+        if(empty()) return -1;
+        int val = q1.front();
+        q1.pop();
+        return val;
+    }
+    
+    int top() {
+        if(empty()) return -1;
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+
+// using one queue
+
 class MyStack {
 public:
 queue<int>q;
