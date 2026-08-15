bbffbe8909c70f2f4d52174e8d473264d0919f4f
Prince <kumarsinghprince650@gmail.com>
2026-08-15 23:06:16 +0530

    Implement DFS for course scheduling validation

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -34,3 +34,29 @@ public:
         return topo.size() == v;
     }
 };
+
+// using DFS
+class Solution {
+public:
+    bool dfs(vector<vector<int>>&adj,vector<int>&status,int course){
+      if(status[course] == 2) return true;
+      if(status[course] == 1) return false;
+      status[course] = 1;
+      for(int next : adj[course]){
+        if(!dfs(adj,status,next)) return false;
+      }
+      status[course] = 2;
+      return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        for(auto &p : prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
+        vector<int>complete(numCourses,0);
+        for(int i = 0;i<numCourses;i++){
+            if(!dfs(adj,complete,i)) return false;
+        }
+        return true;
+    }
+};
