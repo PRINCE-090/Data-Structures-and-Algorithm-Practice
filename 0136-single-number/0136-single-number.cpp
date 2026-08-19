0b88c551c3d96ab6aa0dc895a67d5dc376827e59
Prince <24BAI70733@cuchd.in>
2026-08-19 22:02:04 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.22%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      map<int,int>mp;
-      for(int num : nums){
-        mp[num]++;
-      }
-      for(auto it : mp){
-        if(it.second == 1){
-          return it.first;
+        int xorr = 0;
+        for(int i = 0;i<nums.size();i++){
+            xorr ^= nums[i];
         }
-      }
-      return -1;
-    }
-};
-
-
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-     int xorr = 0;
-     for(int i = 0;i<nums.size();i++){
-      xorr = nums[i] ^ xorr;
-     }
-     return xorr;
+        return xorr;
     }
 };
