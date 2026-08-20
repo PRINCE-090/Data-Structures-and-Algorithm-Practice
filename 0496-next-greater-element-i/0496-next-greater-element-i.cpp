2edc7ecba3691ad1d25e686d8d72496e5c3aca86
Prince <24BAI70733@cuchd.in>
2026-08-20 15:31:25 +0530

    Time: 0 ms (100%), Space: 12.6 MB (76.43%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        vector<int>arr;
+        unordered_map<int,int>mp;
+        for(int i = nums2.size()-1; i >= 0;i--){
+               while(!st.empty() && nums2[i] > st.top()){
+                st.pop();
+             }
+            if(!st.empty()){
+                mp[nums2[i]] = st.top();
+            }
+            else{
+                mp[nums2[i]] = -1;
+            }
+            st.push(nums2[i]);
+        }
+         for(int i = 0;i<nums1.size();i++){
+            arr.push_back(mp[nums1[i]]);
+         }
+        return arr;
+    }
+};
