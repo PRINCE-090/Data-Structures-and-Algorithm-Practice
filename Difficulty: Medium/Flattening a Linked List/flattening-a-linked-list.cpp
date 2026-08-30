174750873e1b6beb9328b2d921df1780dd19f194
Prince <kumarsinghprince650@gmail.com>
2026-08-30 21:25:33 +0530

    Add recursive flattening for linked list
    Implemented a recursive function to flatten a linked list with bottom pointers. Added a merge function to combine two lists.

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,3 +1,52 @@
+// using recursion 
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* bottom;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+        bottom = nullptr;
+    }
+};*/
+   Node * mergeLists(Node *list1, Node *list2){
+       Node *dummyNode = new Node(-1);
+       Node *res = dummyNode;
+       while(list1  != NULL && list2 != NULL){
+           if(list1->data <= list2->data){
+               res->bottom = list1;
+               res = list1;
+               list1 = list1->bottom;
+           }
+           else {
+               res->bottom = list2;
+               res = list2;
+               list2 = list2->bottom;
+           }
+           res->next = NULL;
+       }
+       if(list1) res->bottom = list1;
+       else res->bottom = list2;
+       return dummyNode->bottom;
+       
+   }
+class Solution {
+  public:
+    Node* flatten(Node* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+      Node* newList =  flatten(head->next);
+        return mergeLists(head,newList);
+        
+    }
+};
+
+
+// using min heap
+
 /* Structure of Linked List Node
 class Node {
 public:
