e5a66fcc1147edbafb78e440bdb3848c6bc9443a
Prince <24BAI70733@cuchd.in>
2026-09-08 14:12:28 +0530

    Time: 43 ms (21.53%), Space: 47.5 MB (38.42%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int>freq(26,0),nextavailable(26,0);
-        for(char ch : tasks){
-            freq[ch - 'A']++;
+       int freq[26] = {0};
+       for(char ch : tasks){
+        freq[ch-'A']++;
+       }
+       priority_queue<int>pq;
+       for(int i = 0;i<26;i++){
+        if(freq[i] > 0){
+            pq.push(freq[i]);
         }
-        int time = 0, completed = 0, total = tasks.size();
-        while(completed < total){
-            int selected = -1;
-            int maxfreq = 0;
-            for(int i = 0;i<26;i++){
-              if(freq[i] > 0 && nextavailable[i] <= time){
-                if(freq[i] > maxfreq){
-                    maxfreq = freq[i];
-                    selected = i;
-                }
-              }
+       }
+       int time = 0;
+       while(!pq.empty()){
+        int cycle = n+1;
+        vector<int>store;
+        int taskcount = 0;
+
+        while(cycle-- && !pq.empty()){
+            if(pq.top() > 1){
+                store.push_back(pq.top()-1);
             }
-            if(selected != -1){
-                freq[selected]--;
-                nextavailable[selected] = time + n +1;
-                completed++;
-            }
-            time++;
+            pq.pop();
+            taskcount++;
         }
-        return time;
+        for(int x : store){
+            pq.push(x);
+        }
+        time += (pq.empty() ? taskcount : n+1);
+       }
+       return time;
     }
 };
