ab7bc0ccb491fa72090eb1195ce295429cdc16cb
Prince <24BAI70733@cuchd.in>
2026-09-12 22:42:46 +0530

    Time: 0 ms (100%), Space: 52.4 MB (57.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int reach = 0;
-        int n = nums.size();
-        for(int i = 0;i<nums.size();i++){
-            if(reach < i) return false;
-             reach = max(reach,nums[i]+i);
-             if(reach >= n-1) return true;
-        }
-        return false;
+      int reach = 0;
+      for(int i = 0;i<nums.size();i++){
+        if(reach < i) return false;
+        reach = max(reach,nums[i]+i);
+        if(reach >= nums.size()-1) return true;
+      }
+      return true;
     }
 };
