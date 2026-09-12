63c5518760672bf8774821e43089ca47463fe913
Prince <24BAI70733@cuchd.in>
2026-09-12 22:50:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.3%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <=1) return n;
+        int prev = 1, prev2 = 0;
+        for(int i = 2;i<=n;i++){
+            int sum = prev + prev2;
+            prev2 = prev;
+            prev = sum;
+        }
+        return prev;
+    }
+};
