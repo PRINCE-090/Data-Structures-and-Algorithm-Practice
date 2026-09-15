4405de438e8d6a209a96f4df4d0690b9e2ac2e9d
Prince <prince902390@gmail.com>
2026-09-15 15:12:53 +0530

    Implement minCost function using tabulation and optimization

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -16,3 +16,43 @@ class Solution {
         return mincost(n-1,height,dp);
     }
 };
+
+// Tabulation 
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+      int n = height.size();
+      vector<int>dp(n+1,-1);
+        dp[0] = 0;
+        for(int i = 1;i<n;i++){
+            int fs = dp[i-1] + abs(height[i] - height[i-1]);
+            int ss = INT_MAX;
+            if(i > 1){
+                ss = dp[i-2] + abs(height[i] - height[i-2]);
+            }
+            dp[i] = min(fs,ss);
+        }
+        return dp[n-1];
+    }
+
+// Space optimization 
+
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+      int n = height.size();
+      int prev = 0,prev2 = 0;
+            for(int i = 1;i<n;i++){
+            int fs = prev + abs(height[i] - height[i-1]);
+            int ss = INT_MAX;
+            if(i > 1){
+                ss = prev2 + abs(height[i] - height[i-2]);
+            }
+            int curr = min(fs,ss);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
+};
