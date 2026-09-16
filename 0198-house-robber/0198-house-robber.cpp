20dcb641ecaf1edd31194675993c3d9768567303
Prince <24BAI70733@cuchd.in>
2026-09-16 20:23:50 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int maxmoney(int idx,vector<int>&nums,vector<int>&dp){
-        if(idx < 0) return 0;
-        if(idx == 0) return nums[idx];
-        if(dp[idx] != -1) return dp[idx];
-        int take = nums[idx] + maxmoney(idx-2,nums,dp);
-        int notTake = 0 + maxmoney(idx-1,nums,dp);
-        
-        return dp[idx] = max(take,notTake);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return maxmoney(n-1,nums,dp);
+        vector<int>dp(n);
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2;i<n;i++){
+            int take = nums[i] + dp[i-2];
+            int notTake = dp[i-1];
+
+            dp[i] = max(take,notTake);
+        }
+        return dp[n-1];
     }
 };
