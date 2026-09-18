646a9b384dd18bd366b17f05afc6ac3775e1aba4
Prince <prince902390@gmail.com>
2026-09-18 13:38:34 +0530

    Implement maximumPoints with memoization and optimization
    Refactored maximumPoints function using tabulation and space optimization techniques. Added memoization and improved performance.

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,3 +1,5 @@
+// Memoization
+
 class Solution {
   public:
     int maxpoint(int day,int last,vector<vector<int>>&mat,vector<vector<int>>&dp){
@@ -24,6 +26,65 @@ class Solution {
         int n = mat.size();
         vector<vector<int>>dp(n,vector<int>(4,-1));
         return maxpoint(n-1,3,mat,dp);
+        
+    }
+};
+
+// tabulation 
+class Solution {
+  public:
+    int maximumPoints(vector<vector<int>>& mat) {
+        int n = mat.size();
+        vector<vector<int>>dp(n,vector<int>(4,0));
+        dp[0][0] = max(mat[0][1],mat[0][2]);
+        dp[0][1] = max(mat[0][0],mat[0][2]);
+        dp[0][2] = max(mat[0][1],mat[0][0]);
+        dp[0][3] = max({mat[0][1],mat[0][2],mat[0][0]});
+        
+        for(int day = 1;day<n;day++){
+            for(int last = 0;last<4;last++){
+                dp[day][last] = 0;
+                
+                for(int task = 0;task<3;task++){
+                    if(task!=last){
+                        int point = mat[day][task] + dp[day-1][task];
+                        dp[day][last] = max(dp[day][last],point);
+                    }
+                }
+            }
+        }
+        
+        return dp[n-1][3];
+        
+    }
+};
+
+// Space optimation 
+class Solution {
+  public:
+    int maximumPoints(vector<vector<int>>& mat) {
+        int n = mat.size();
+       vector<int>prev(4,0);
+        prev[0] = max(mat[0][1],mat[0][2]);
+        prev[1] = max(mat[0][0],mat[0][2]);
+        prev[2] = max(mat[0][1],mat[0][0]);
+        prev[3] = max({mat[0][1],mat[0][2],mat[0][0]});
+        
+        for(int day = 1;day<n;day++){
+            vector<int>temp(4,0);
+            for(int last = 0;last<4;last++){
+                temp[last] = 0;
+                
+                for(int task = 0;task<3;task++){
+                    if(task!=last){
+                        temp[last] = max(temp[last],mat[day][task]+prev[task]);
+                    }
+                }
+            }
+            prev = temp;
+        }
+        
+        return prev[3];
         
     }
 };
