768ef2a33031cdfad341e3f3182e170c2b73b8e2
Prince <24BAI70733@cuchd.in>
2026-09-21 22:17:06 +0530

    Time: 3 ms (13.19%), Space: 13.4 MB (44.23%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-     int minpathsum(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&dp,int n){
-        if(row == n-1) return grid[row][col];
-        if(dp[row][col] != -1e9) return dp[row][col];
-
-        int down = grid[row][col] + minpathsum(row + 1,col,grid,dp,n);
-        int diagonal = grid[row][col] + minpathsum(row + 1, col + 1 ,grid,dp,n);
-        return dp[row][col] = min(down,diagonal);
-     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1e9));
-        return minpathsum(0,0,triangle,dp,n);
+        vector<vector<int>>dp(n,vector<int>(n));
+        for(int j = 0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i = n-2;i>=0;i--){
+            for(int j = i;j>= 0;j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
