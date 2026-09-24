cc000f1dd8ee470e9646470fd83dc6a750d9ce51
Prince <prince902390@gmail.com>
2026-09-24 21:14:26 +0530

    Add subset sum problem solutions with optimizations
    Implemented subset sum problem using tabulation and space optimization techniques.

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,3 +1,50 @@
+// Tabulation
+class Solution {
+  public:
+    bool sumequaltoK(int idx,int target,vector<int>&arr,vector<vector<int>>&dp){
+        if(target == 0) return true;
+        if(idx == 0){
+            return arr[idx] == target;
+        }
+        if(dp[idx][target] != -1) return dp[idx][target];
+        bool notTake = sumequaltoK(idx-1,target,arr,dp);
+        bool take = false;
+        if(arr[idx] <= target){
+            take = sumequaltoK(idx-1,target-arr[idx],arr,dp);
+        }
+        return dp[idx][target] = notTake | take;
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return sumequaltoK(n-1,sum,arr,dp);
+    }
+};
+
+// Tabulation
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<bool>>dp(n,vector<bool>(sum+1,0));
+        for(int i = 0;i<n;i++) dp[i][0] = true;
+        if(arr[0] <= sum) dp[0][arr[0]] = true;
+        
+        for(int ind = 1;ind<n;ind++){
+            for(int target = 1;target <= sum;target++){
+                bool notTake = dp[ind-1][target];
+                bool take = false;
+                if(arr[ind] <= target){
+                    take = dp[ind-1][target-arr[ind]];
+                }
+                dp[ind][target] = take || notTake;
+            }
+        }
+        return dp[n-1][sum];
+    }
+};
+
+// Space Optimization
 class Solution {
   public:
     bool isSubsetSum(vector<int>& arr, int sum) {
