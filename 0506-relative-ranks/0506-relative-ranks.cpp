51449a191f5a1a00b86e9c232f7d84663a1654d5
Prince <24BAI70733@cuchd.in>
2026-09-25 15:35:40 +0530

    Time: 3 ms (79.62%), Space: 15.5 MB (77.79%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
-        vector<pair<int,int>>v;
+        priority_queue<pair<int,int>>pq;
         vector<string>ans(n);
         for(int i = 0;i<n;i++){
-            v.push_back({score[i],i});
+           pq.push({score[i],i});
         }
-        sort(v.rbegin(),v.rend());
-        for(int i = 0;i<n;i++){
-            pair<int,int>rank = v[i];
+         int i = 0;
+        while(!pq.empty()){
             if(i == 0){
-                ans[rank.second] = "Gold Medal";
+                ans[pq.top().second] = "Gold Medal";
+                i++;
             }
            else if(i == 1){
-                ans[rank.second] = "Silver Medal";
+                ans[pq.top().second] = "Silver Medal";
+                i++;
             }
            else if(i == 2){
-                ans[rank.second] = "Bronze Medal";
+                ans[pq.top().second] = "Bronze Medal";
+                i++;
             }
             else{
-                ans[rank.second] = to_string(i+1);
+                ans[pq.top().second] = to_string(i+1);
+                i++;
             }
+            pq.pop();
         }
         return ans;
     }
