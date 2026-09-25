0af18683f6336a71cdf49a67d1eea5e59c9d9444
Prince <prince902390@gmail.com>
2026-09-25 15:37:22 +0530

    Implement relative ranks using priority queue
    Added an alternative implementation using a priority queue for finding relative ranks.

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,3 +1,36 @@
+// using vector 
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<pair<int,int>>v;
+        vector<string>ans(n);
+        for(int i = 0;i<n;i++){
+            v.push_back({score[i],i});
+        }
+        sort(v.rbegin(),v.rend());
+        for(int i = 0;i<n;i++){
+            pair<int,int>rank = v[i];
+            if(i == 0){
+                ans[rank.second] = "Gold Medal";
+            }
+           else if(i == 1){
+                ans[rank.second] = "Silver Medal";
+            }
+           else if(i == 2){
+                ans[rank.second] = "Bronze Medal";
+            }
+            else{
+                ans[rank.second] = to_string(i+1);
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+// using priority_queue
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
